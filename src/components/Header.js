64e8f5be0e635e7dbefe9263a8c7b8d7f2a8c384
rahimianarezoo <rahimian.arezoo@gmail.com>
2024-05-31 18:17:18 +0200

    sign out/update users and add profile image

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { auth } from '../utils/firebase';
+import { useNavigate } from 'react-router-dom';
+import { signOut } from "firebase/auth";
+import { useSelector } from 'react-redux';
 
 const Header = () => {
+  // const [isSignOut, setIsSignOut] = useState(false)
+  const user =useSelector((store) => store.user)
+  const navigate = useNavigate();
+  const handleSignOut = () => {
+    signOut(auth).then(() => {
+      navigate('/');
+    }).catch((error) => {
+      // An error happened.
+      navigate('/error');
+    });
+  }
   return (
-    <div className='px-8 py-2  bg-opacity-90 bg-black flex justify-between z-10'>
+    <div className='px-8 py-2 w-screen bg-gradient-to-b from-black bg-opacity-90 absolute flex justify-between z-10'>
         <img className='w-44' 
         src='https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png'
         alt='Netflix-logo' />
-        <div className='mt-5'>
-          <button 
-          className='bg-red-600 px-3 py-2 rounded-md text-white'>
-            Sign in
-          </button>
-        </div>
+    {user && (
+              <div className='mt-5 flex'>
+              <img 
+                className='w-6 h-6 mr-3'
+                alt='user-icon' 
+                src={user?.photoURL}
+              />
+              <div>
+                <button 
+                  className='bg-red-600 px-1 rounded-md text-white py-1'
+                  onClick={handleSignOut}>
+                    Sign out
+                </button>
+              </div>
+            </div>
+    )}
     </div>
   )
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -2,13 +2,17 @@ import React, { useState , useRef } from 'react';
 import Header from '../components/Header';
 import {checkValidData} from '../utils/validate';
 import { auth } from '../utils/firebase';
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword} from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile} from "firebase/auth";
 import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { addUser } from '../utils/userSlice';
 
 const Login = () => {
-  const [isSignInForm,setIsSignInForm] = useState(true)
+  const [isSignInForm,setIsSignInForm] = useState(true);
   const[errorMessage, setErrorMessage] = useState(null);
-  const navigate = useNavigate()
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const name = useRef(null);
   const email = useRef(null);
   const password = useRef(null);
 
@@ -25,7 +29,25 @@ const Login = () => {
         .then((userCredential) => {
           // Signed up 
           const user = userCredential.user;
-          navigate("/browse")
+          updateProfile(user, {
+            displayName: name.current.value, 
+            photoURL: "https://avatars.githubusercontent.com/u/32914697?s=400&u=7d0967997407d4fe53d49b6af2c709a66c8883e8&v=4"
+          }).then(() => {
+            // Profile updated!
+            const {uid, email, displayName, photoURL} = auth.currentUser;
+              dispatch(
+                addUser({ 
+                  uid: uid, 
+                  email: email, 
+                  displayName: displayName, 
+                  photoURL: photoURL 
+                })
+              );
+            navigate("/browse");
+          }).catch((error) => {
+            // An error occurred
+            setErrorMessage(error.message)
+          });
         })
         .catch((error) => {
           const errorCode = error.code;
@@ -68,6 +90,7 @@ const Login = () => {
           className='absolute rounded-lg w-4/12 my-36 mx-auto right-0 left-0 text-white p-12 bg-black bg-opacity-80'>
           <h1 className='font-bold text-3xl py-4'>{isSignInForm ? "Sign In" : "Sign Up"}</h1>
           {!isSignInForm && <input 
+            ref={name}
             className='p-4 my-4 w-full bg-gray-800'
             type='text' 
             placeholder='Full Name'>
